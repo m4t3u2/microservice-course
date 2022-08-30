7e28946d6d800334fef967b715556229f2085928
Mateus Simon <ms.mateus@hotmail.com>
2022-08-29 22:07:13 -0300

    Feign enabled

--- hr-payroll/src/main/java/br/dev/simon/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/dev/simon/hrpayroll/services/PaymentService.java
@@ -1,32 +1,22 @@
 package br.dev.simon.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.dev.simon.hrpayroll.entities.Payment;
 import br.dev.simon.hrpayroll.entities.Worker;
+import br.dev.simon.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
 	// There is no repository, only mocked data.
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(Long workerId, Integer days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", "" + workerId);
-
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
