64de54c9d3d7c1febe6bf7279be16fc0250b22aa
Mateus Simon <ms.mateus@hotmail.com>
2022-10-10 22:45:17 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/br/dev/simon/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.simon.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
